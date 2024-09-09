77fa6b1e2c1dba2b8271968ebe3568d8e36fd193
Arsh <120127753+ArshdevSinghji@users.noreply.github.com>
2024-09-09 12:42:22 +0530

    first Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+    override fun onStart(){
+        super.onStart()
+        val toast=Toast.makeText(applicationContext,"onStart Called",Toast.LENGTH_LONG)
+        toast.show() //A toast provides simple feedback about an operation in a small popup
+    }
+    //applicationContext: represents the current state of the application
+    override fun onRestart() {
+        super.onRestart()
+        val toast=Toast.makeText(this,"onRestart Called",Toast.LENGTH_LONG).show()
+    }
+    override fun onPause(){
+        super.onPause()
+        val toast=Toast.makeText(applicationContext,"onPause Called",Toast.LENGTH_LONG).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        val toast=Toast.makeText(applicationContext,"onResume Called",Toast.LENGTH_LONG).show()
+    }
+    override fun onStop() {
+        super.onStop()
+        val toast=Toast.makeText(applicationContext,"onStop Called",Toast.LENGTH_LONG).show()
+        Log.d("MainActivity","onStopCalled")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        val toast=Toast.makeText(applicationContext,"onDestroy Called",Toast.LENGTH_LONG).show()
+    }
+}
