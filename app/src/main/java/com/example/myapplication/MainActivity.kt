bae34c0c9880bdd4ab12d46ce4e93e79d0e9bd5b
Arsh <120127753+ArshdevSinghji@users.noreply.github.com>
2024-09-11 12:29:13 +0530

    Created a simple login page that displays user username and password

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,32 +16,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val username=findViewById<EditText>(R.id.etUsername)
+        val password=findViewById<TextView>(R.id.etPassword)
+        val output=findViewById<TextView>(R.id.tvOutput)
+        val btn=findViewById<Button>(R.id.btnLogin)
+        btn.setOnClickListener{
+            val name=username.text.toString()
+            val pass=password.text.toString()
+            output.text="My Username is $name and Password is $pass"
+        }
     }
-    override fun onStart(){
-        super.onStart()
-        val toast=Toast.makeText(applicationContext,"onStart Called",Toast.LENGTH_LONG)
-        toast.show() //A toast provides simple feedback about an operation in a small popup
-    }
-    //applicationContext: represents the current state of the application
-    override fun onRestart() {
-        super.onRestart()
-        val toast=Toast.makeText(this,"onRestart Called",Toast.LENGTH_LONG).show()
-    }
-    override fun onPause(){
-        super.onPause()
-        val toast=Toast.makeText(applicationContext,"onPause Called",Toast.LENGTH_LONG).show()
-    }
-    override fun onResume() {
-        super.onResume()
-        val toast=Toast.makeText(applicationContext,"onResume Called",Toast.LENGTH_LONG).show()
-    }
-    override fun onStop() {
-        super.onStop()
-        val toast=Toast.makeText(applicationContext,"onStop Called",Toast.LENGTH_LONG).show()
-        Log.d("MainActivity","onStopCalled")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        val toast=Toast.makeText(applicationContext,"onDestroy Called",Toast.LENGTH_LONG).show()
-    }
+//    override fun onStart(){
+//        super.onStart()
+//        val toast=Toast.makeText(applicationContext,"onStart Called",Toast.LENGTH_LONG)
+//        toast.show() //A toast provides simple feedback about an operation in a small popup
+//    }
+//    //applicationContext: represents the current state of the application
+//    override fun onRestart() {
+//        super.onRestart()
+//        val toast=Toast.makeText(this,"onRestart Called",Toast.LENGTH_LONG).show()
+//    }
+//    override fun onPause(){
+//        super.onPause()
+//        val toast=Toast.makeText(applicationContext,"onPause Called",Toast.LENGTH_LONG).show()
+//    }
+//    override fun onResume() {
+//        super.onResume()
+//        val toast=Toast.makeText(applicationContext,"onResume Called",Toast.LENGTH_LONG).show()
+//    }
+//    override fun onStop() {
+//        super.onStop()
+//        val toast=Toast.makeText(applicationContext,"onStop Called",Toast.LENGTH_LONG).show()
+//        Log.d("MainActivity","onStopCalled")
+//    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        val toast=Toast.makeText(applicationContext,"onDestroy Called",Toast.LENGTH_LONG).show()
+//    }
 }
